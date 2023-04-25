5b15986db5c92b5c831e98167358d200b919e5aa
SinezioJr <51363662+SinezioJr@users.noreply.github.com>
2023-04-25 09:25:59 -0300

    feat: :sparkles: update interfaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
-#include <Arduino.h>
+#include <interface.h>
+#include <joystic.h>
+#include <craneController.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Joystick joystick = Joystick(12, 13);
+
+CraneParams crane;
+
+volatile bool selectFlag = false;
+
+void IRAM_ATTR changeSelectFlag()
+{
+  Serial.println("Pres");
+  selectFlag = !selectFlag;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  pinMode(14, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(14), changeSelectFlag, RISING);
+
+  Serial.begin(115200);
+  joystick.begin();
+
+  initDisplay();
+  drawVerticalVission();
+
+  drawCrane(crane.X, crane.Y);
+  drawSuperiorVision(crane.angle);
+}
+
+void loop()
+{
+  craneController(&crane, joystick.readX(), joystick.readY(), selectFlag);
+  if (selectFlag)
+  {
+    drawCrane(crane.X, crane.Y);
+  }
+  else
+  {
+    drawSuperiorVision(crane.angle);
+  }
 }
